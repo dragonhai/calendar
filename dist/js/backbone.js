9435f2c785e9080b17708f34a57cbba06b44da90
dragon.hai <dragon.hai@1pac.jp>
2016-04-21 16:00:29 +0700

    show calendar

--- dist/js/backbone.js
+++
@@ -1,1 +0,0 @@
-!function(a){define("ccalendar",["backbone","calendar"],a)}(function(a,b){"use strict";var c=a.View.extend({initialize:function(){this.render()},render:function(){var a=new b;a.init("../js/data/events.json")}});return c});
